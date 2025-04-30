543b5532ed9768a8d3b9f28ba448ee15b5cc3fb1
Erkebulan Abdisattar <abdisattarerkebulan@gmail.com>
2025-05-01 03:37:18 +0500

    Commit

--- src/MyTestingClass.java
+++ src/MyTestingClass.java
@@ -24,13 +24,6 @@ public class MyTestingClass {
         return result;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        MyTestingClass that = (MyTestingClass) obj;
-        return id == that.id;
-    }
 
     @Override
     public String toString() {
